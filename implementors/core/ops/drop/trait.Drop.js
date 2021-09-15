474ac8898233fa41b3893d4a77ce33f7387f3b42
slonopotamus <slonopotamus@users.noreply.github.com>
2021-09-15 09:14:46 +0000

    deploy: ef49450b1f876fa72320d157b06cbfeb55782d72

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["ipc"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ipc/struct.Guard.html\" title=\"struct ipc::Guard\">Guard</a>&lt;'a&gt;","synthetic":false,"types":["ipc::Guard"]}];
+implementors["ipc"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ipc/struct.Guard.html\" title=\"struct ipc::Guard\">Guard</a>&lt;'a&gt;","synthetic":false,"types":["ipc::Guard"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
