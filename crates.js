795f37441ce2c96e4bba73ed47155c142a25536b
slonopotamus <slonopotamus@users.noreply.github.com>
2025-02-26 10:22:19 +0000

    deploy: e8d76ee36146d4548d18ba8480bf5b5a2f116eac

--- crates.js
+++ crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["ipc","libc"];
+//{"start":21,"fragment_lengths":[5,7]}
--- src-files.js
+++ src-files.js
@@ -1,5 +1,3 @@
-var srcIndex = JSON.parse('{\
-"ipc":["",[],["lib.rs","unix.rs"]],\
-"libc":["",[["unix",[["linux_like",[["linux",[["arch",[["generic",[],["mod.rs"]]],["mod.rs"]],["gnu",[["b64",[["x86_64",[],["align.rs","mod.rs","not_x32.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["align.rs","mod.rs","non_exhaustive.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["fixed_width_ints.rs","lib.rs","macros.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[["ipc",["",[],["lib.rs","unix.rs"]]],["libc",["",[["unix",[["linux_like",[["linux",[["arch",[["generic",[],["mod.rs"]]],["mod.rs"]],["gnu",[["b64",[["x86_64",[],["mod.rs","not_x32.rs"]]],["mod.rs"]]],["mod.rs"]]],["mod.rs"]]],["mod.rs"]]],["mod.rs"]]],["lib.rs","macros.rs","primitives.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[36,256]}
